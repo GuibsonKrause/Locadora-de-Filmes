64affad033421f060c8805764c0a672cba22d744
bianc <bianc@172.16.38.102>
2018-03-27 10:30:41 -0300

    Hiber

--- src/java/model/domain/Ator.java
+++ src/java/model/domain/Ator.java
@@ -1,5 +1,13 @@
 package model.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,9 +18,48 @@ package model.domain;
  *
  * @author 2016122760198
  */
-public class Ator {
 
-    public Ator() {
+
+@Entity
+public class Ator implements Serializable{
+   
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    private String nome;
+    
+
+    public Ator(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
     }
+
+    public Ator(String nome) {
+        this.nome = nome;
+    }
+
+    public Ator() 
+    {
+        
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
     
 }
